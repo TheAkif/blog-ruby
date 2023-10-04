ec7af5e6cc935a2289d40370987a5032086189d2
akif hussain <akifhussain@Macbooks-MacBook-Pro.local>
2023-10-04 15:35:41 +0500

    Added transcript route and changes in views.

---
+++ app/controllers/transcripts_controller.rb
@@ -0,0 +1,4 @@
+class TranscriptsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/transcripts', to: "transcripts#index"
+  root "articles#index"
+
   get "/articles", to: "articles#index"
 
 end
