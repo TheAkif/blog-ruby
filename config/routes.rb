928be779af524e6b0ca00770b526465a9c3e397e
akif hussain <akifhussain@Macbooks-MacBook-Pro.local>
2023-10-05 18:10:56 +0500

    Added comments resource in articles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :articles
   get '/transcripts', to: "transcripts#index"
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  resources :users
 end
