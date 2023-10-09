ec2eac6b281cf860ec9dccf0c539a8f82bd7c66f
akif hussain <akifhussain@Macbooks-MacBook-Pro.local>
2023-10-09 13:16:05 +0500

    Adding devise to application.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
-class Article < ApplicationRecord
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :comments
-    
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
-  end
-  
+end
